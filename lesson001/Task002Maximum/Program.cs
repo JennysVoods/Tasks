f7e4d6dbe1aaa5b0d4ceb56263e12cbd0dd8c2de
VlasovaOlga <olchik-kosha296@mail.ru>
2022-04-20 01:00:30 +0300

    Task 002 Maximum

---
+++ lesson001/Task002Maximum/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberA = 22; //предположили что работаем с целыми числами
+int numberB = 3;
+int numberC = 9;
+int max = numberA;
+
+if (numberA > max)
+    max = numberA;
+if (numberB > max)
+    max = numberB;
+if (numberC > max)
+    max = numberC;
+
+Console.Write("max = ");
+Console.WriteLine(max);
