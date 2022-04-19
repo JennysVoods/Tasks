4c498e194dc1ad8c8948888f443ad5ee3a2a0eab
VlasovaOlga <olchik-kosha296@mail.ru>
2022-04-20 00:12:23 +0300

    Task 001 Max Min

---
+++ lesson001/Task001MinMax/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberA = -9; //предположили что работаем с целыми числами
+int numberB = -3;
+int max = numberA;
+int min = numberA;
+
+if (numberA > numberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(max = numberA);
+    Console.Write("min = ");
+    Console.WriteLine(min = numberB);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(max = numberB);
+    Console.Write("min = ");
+    Console.WriteLine(min = numberA);
+}
