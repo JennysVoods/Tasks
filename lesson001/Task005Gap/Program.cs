49ff5e0b6bc206a6e614a54d56855db390edfd4d
VlasovaOlga <olchik-kosha296@mail.ru>
2022-04-20 15:05:21 +0300

    Task 005 Gap

---
+++ lesson001/Task005Gap/Program.cs
@@ -0,0 +1,22 @@
+﻿int numberN = -4; //предположили что работаем с целыми числами
+
+if (numberN > 0)
+{
+    int gupN = -numberN;
+
+    while (gupN <= numberN)
+    {
+        Console.WriteLine(gupN);
+        gupN++;
+    }
+}
+else
+{
+    int gupN = -numberN;
+
+    while (numberN <= gupN)
+    {
+        Console.WriteLine(numberN);
+        numberN++;
+    }
+}
