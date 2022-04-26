63d2816d7fce2b6b0f8bbd5955f62ac9f4343a41
VlasovaOlga <olchik-kosha296@mail.ru>
2022-04-26 12:02:10 +0300

    Task 001 Random Max

---
+++ lesson002/Task001_RandomMax/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetNumber()
+{
+    int number = new Random().Next(10, 100);
+
+    int a = number / 10;
+    int b = number % 10;
+
+    //Console.WriteLine($"number: {number}  a: {a}  b: {b}");
+
+    int max = a;
+    if (b > max)
+        max = b;
+    return max;
+}
+System.Console.WriteLine(GetNumber());
