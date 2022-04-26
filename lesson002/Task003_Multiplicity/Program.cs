38e6f2566f084df90a2d2232d715815b2882b67c
VlasovaOlga <olchik-kosha296@mail.ru>
2022-04-27 00:29:45 +0300

    Task 003 Multiplicity

---
+++ lesson002/Task003_Multiplicity/Program.cs
@@ -0,0 +1,18 @@
+﻿int Multiplicity(int numberA, int numberB)
+{
+    int number = 0;
+
+    if (numberA % numberB == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        number = numberA % numberB;
+        Console.WriteLine($"Не кратно, остаток {number}");
+    }
+    return;
+}
+
+int digitA = 16;
+int digitB = 4;
