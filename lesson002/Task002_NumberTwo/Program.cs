9ff8a6bcbad1571f005fddef50fdc3f5077e40b0
VlasovaOlga <olchik-kosha296@mail.ru>
2022-04-26 22:41:17 +0300

    Task 002 Number Two

---
+++ lesson002/Task002_NumberTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿int NumberTwo(int number)
+{
+    int digit = -1;
+    if (number > 99 && number < 1000)
+    {
+        digit = (number / 10) % 10;
+    }
+    return digit;
+}
+
+int ThreeDigitNumber = 357;
+System.Console.WriteLine(NumberTwo(ThreeDigitNumber));
