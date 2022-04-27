618eea4bfc133faabc7992747be0e2189f0676bf
VlasovaOlga <olchik-kosha296@mail.ru>
2022-04-28 00:55:47 +0300

    Task 004 Even To Number

--- lesson001/Task004EvenToNumber/Program.cs
+++ lesson001/Task004EvenToNumber/Program.cs
@@ -1,16 +1,12 @@
 ﻿int numberN = 8; //предположили что работаем с целыми числами
 
-int numberA = 1;
+int numberA = 2;
 
 while (numberA <= numberN)
 {
     if (numberA % 2 == 0)
     {
         Console.WriteLine(numberA);
-        numberA++;
     }
-    else
-    {
-        numberA++;
-    }
+    numberA++;
 }
