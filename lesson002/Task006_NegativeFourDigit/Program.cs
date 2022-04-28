fef65eff94f245a03c94ffe669a02339244b3b09
VlasovaOlga <olchik-kosha296@mail.ru>
2022-04-28 22:45:06 +0300

    Task 006 Negative Four Digit

---
+++ lesson002/Task006_NegativeFourDigit/Program.cs
@@ -0,0 +1,15 @@
+﻿void NegativeFourDigit(int number)
+{
+    int digit = 0;
+    if(number > -10000 && number < -999)
+    {
+        digit = -number;
+        digit = (digit / 10) % 10;
+        Console.WriteLine(digit);
+    }
+    else
+    {
+        Console.WriteLine("Number entered incorrectly");
+    }
+}
+NegativeFourDigit(-9118);
