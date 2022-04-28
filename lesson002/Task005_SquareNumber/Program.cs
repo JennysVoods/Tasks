9cf8a6c5af45b5a0621fe7c101e06aecf13ff7a8
VlasovaOlga <olchik-kosha296@mail.ru>
2022-04-28 21:23:00 +0300

    Task 005 Square Number

---
+++ lesson002/Task005_SquareNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿void SquareNumber(int numberA, int numberB)
+{
+    if(numberA > numberB)
+    {
+        if (numberB * numberB == numberA)
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    }
+    else
+    {
+        if (numberA * numberA == numberB)
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    }
+}
+
+SquareNumber(8,9);
