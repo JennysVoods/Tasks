2ce0c65f6dbc8f02392fc840bfa8df257861cc09
VlasovaOlga <olchik-kosha296@mail.ru>
2022-04-28 19:33:53 +0300

    Task 004 Multiple Two

---
+++ lesson002/Task004_MultipleTwo/Program.cs
@@ -0,0 +1,16 @@
+﻿void MultipleTwo(int number)
+{
+    int numberA = 7;
+    int numberB = 23;
+    if(number % numberA == 0 && number % numberB == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+MultipleTwo(161);
+
+
