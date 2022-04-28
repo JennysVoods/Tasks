78de442339f5d9468aa04bd1def9532dad783b3e
VlasovaOlga <olchik-kosha296@mail.ru>
2022-04-29 00:41:00 +0300

    Task 007 Third Number

---
+++ lesson002/Task007_ThirdNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿void ThirdNumber(int number)
+{
+    if (number > 99)
+    {
+        string text = number.ToString();
+        Console.WriteLine(text[2]);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
+ThirdNumber(32679);
