c8f35fe237627ead4f9987159cb0d4dd1aaf853c
VlasovaOlga <olchik-kosha296@mail.ru>
2022-05-02 15:43:25 +0300

    Task 008 Day Off

---
+++ lesson002/Task008_DayOff/Program.cs
@@ -0,0 +1,20 @@
+﻿void DayOff(int day)
+{
+    if (day > 0 && day < 8)
+    {
+        if (day == 6 || day == 7)
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Number entered incorrectly");
+    }
+}
+
+DayOff(1);
