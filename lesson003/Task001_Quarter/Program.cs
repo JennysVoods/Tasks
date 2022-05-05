6c071792b65a20f2c6f3218d263a9794600c5089
VlasovaOlga <olchik-kosha296@mail.ru>
2022-05-05 23:33:39 +0300

    Task 001 Quarter

---
+++ lesson003/Task001_Quarter/Program.cs
@@ -0,0 +1,28 @@
+﻿int Quarter(int x, int y)
+{
+    if (x != 0 && y != 0)
+    {
+        int num = 0;
+        if (x > 0 && y > 0)
+            num = 1;
+        if (x > 0 && y < 0)
+            num = 2;
+        if (x < 0 && y < 0)
+            num = 3;
+        if (x < 0 && y > 0)
+            num = 4;
+        return num;
+    }
+    else
+        return -1;
+}
+int number = Quarter(-1,2); 
+
+if (number == -1)
+{
+    Console.WriteLine("Number entered incorrectly");
+}
+else
+{
+    Console.WriteLine(number);
+}
