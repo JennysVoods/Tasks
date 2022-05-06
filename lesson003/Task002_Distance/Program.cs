b1ce62bee5bf318bcf23cd2cfc882de043c2a1dd
VlasovaOlga <olchik-kosha296@mail.ru>
2022-05-07 00:32:53 +0300

    Task 002 Distance

---
+++ lesson003/Task002_Distance/Program.cs
@@ -0,0 +1,21 @@
+﻿double DistancePoints(double Xa, double Ya, double Xb, double Yb)
+{
+    double maxX = Xa;
+    double minX = Xb;
+    double maxY = Ya;
+    double minY = Yb;
+    if (Xb > Xa)
+    {
+        maxX = Xb;
+        minX = Xa;
+    }
+    if (Yb > Ya)
+    {
+        maxY = Yb;
+        minY = Ya;
+    }
+    return Math.Sqrt((Math.Pow((maxX - minX), 2)) + (Math.Pow((maxY - minY), 2)));
+}
+
+
+Console.WriteLine(DistancePoints(7, -5, 1, -1));
