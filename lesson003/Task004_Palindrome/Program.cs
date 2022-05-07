7d799d19567118213b4679c3a3cb926c54d0b476
VlasovaOlga <olchik-kosha296@mail.ru>
2022-05-08 00:00:44 +0300

    Task 004 Palindrome наброски

---
+++ lesson003/Task004_Palindrome/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+// 1. Задать число : с клавиатуры/рандом
+// 2. Проверить является ли число 5 значным
+// 3. проверить палиндром или нет
+// 4. Вывести ответ
+
+
+
+
+// int number = 14212;
+
+// int Number()
+// {
+//     int number = new Random().Next(10000, 100000);
+//     return number;
+// }
+// Console.WriteLine(Number());
+
+
+// int FiveNumber(int number)
+// {
+//     if (number > 9999 && number < 100000)
+//         return number;
+//     else
+//         return -1;
+// }
+
+
+// int num = FiveNumber(Number());
+if (num == -1)
+{
+    Console.WriteLine("Number entered incorrectly");
+}
+
+int num = str.ToString();
+
+
+
+void Palindrom()
+{
+
+}
