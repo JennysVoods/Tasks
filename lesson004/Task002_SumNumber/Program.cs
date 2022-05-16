6101068d574863331a1d98963cb2ac92304d215d
VlasovaOlga <olchik-kosha296@mail.ru>
2022-05-16 17:02:56 +0300

    Task 002 Sum Number

---
+++ lesson004/Task002_SumNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+int SumNumber(int num)
+{
+    string stringNum = num.ToString();
+    int sum = 0;
+    for (int i = 0; i < stringNum.Length; i++)
+    {
+        int numb = Convert.ToInt32(Convert.ToString(stringNum[i]));
+        sum += numb;
+    }
+    return sum;
+}
+Console.WriteLine(SumNumber(1231));
