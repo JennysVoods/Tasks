318181069637138d987971ca77a47d02c6896bc9
VlasovaOlga <olchik-kosha296@mail.ru>
2022-05-16 21:45:42 +0300

    Tasks 001 Even Three Digit

---
+++ lesson005/Tasks001_EvenThreeDigit/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void ThreeDigitArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(99, 1000);
+    }
+}
+
+int EvenNumberArray(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if(numbers[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+
+int[] array = new int[10];
+ThreeDigitArray(array);
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.Write($"{array[i]} ");
+// }
+int EvenCount = EvenNumberArray(array);
+// Console.WriteLine();
+Console.WriteLine($"Number of even numbers = {EvenCount}");
